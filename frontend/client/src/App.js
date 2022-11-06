c7dbfb48ccb2e8e9f184e38d9e94d993ed3d6048
GeorgeOrzanescu <george.asro@gmail.com>
2022-11-06 11:02:09 +0200

    Experimenting with context

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import NavigationBar from "./components/NavigationBar"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from "./components/Home";
@@ -8,7 +7,56 @@ import Playlist from "./components/Playlist";
 import AddSong from "./components/AddSong";
 import Profile from "./components/Profile";
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import AuthService from "./services/authService";
+import {useState} from "react";
+import LoginAndRegisterModal from "./components/LoginAndRegisterModal";
+import ButtonAll from "./components/ButtonAll";
+import AppContext from "./context/AppContext";
+
 function App() {
+    const [isLoggedIn,setLoggedIn] = useState(false);
+    const [originModal,setOriginModal] = useState("");
+    const [modalShow, setModalShow] = useState(false);
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChangeUser = (event) => {
+        setUser(event.target.value);
+    }
+
+    const onChangePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        switch (originModal) {
+            case "register": {
+                const data = await AuthService.register(user, password);
+                if (data) {
+                    console.log(data);
+                    setLoggedIn(true);
+                    alert("Registration was successful")
+                }
+                break;
+            }
+            case "login": {
+                const data = await AuthService.login(user,password);
+                if (data) {
+                    console.log(data);
+                    setLoggedIn(true);
+                    alert("Login was successful")
+                }
+                break;
+            }
+        }
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,9 +73,65 @@ function App() {
           WEB PROJECT
         </a>
       </header>
+        <AppContext.Provider value={{
+            user:user
+        }}>
         <BrowserRouter>
             <div>
-                <NavigationBar/>
+                <Navbar bg="primary" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="/">HOME</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/profile">Profile</Nav.Link>
+                            <Nav.Link href="/playlist">Playlist</Nav.Link>
+                            <Nav.Link href="/addsong">Add song</Nav.Link>
+                        </Nav>
+
+                        {!isLoggedIn ?
+                            <ButtonAll
+                                className={"login-btn"}
+                                state={false}
+                                handler={() => {
+                                    setOriginModal("login");
+                                    setModalShow(true);
+                                }}
+                                message={"Login"}/> :
+                            <ButtonAll
+                                className={"login-btn"}
+                                state={false}
+                                handler={() => {
+                                    setLoggedIn(false);
+                                }}
+                                message={"Logout"}/>
+                        }
+                        {!isLoggedIn ?
+                            <ButtonAll
+                                className={"register-btn"}
+                                state={false}
+                                handler={() => {
+                                    setOriginModal("register");
+                                    setModalShow(true);
+                                }}
+                                message={"Register"}/> :
+                            <ButtonAll
+                                className={"register-btn"}
+                                state={true}
+                                handler={() => {
+                                    setOriginModal("register");
+                                    setModalShow(true);
+                                }}
+                                message={"Register"}/>
+                        }
+
+                        <LoginAndRegisterModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                            onSubmit={submitHandler}
+                            onChangePassword ={onChangePassword}
+                            onChangeUser ={onChangeUser}
+                        />
+                    </Container>
+                </Navbar>
             </div>
             <Routes>
                 <Route path="/" element={<Home/>} />
@@ -36,6 +140,8 @@ function App() {
                 <Route path="/addsong" element={<AddSong/>} />
             </Routes>
         </BrowserRouter>
+        </AppContext.Provider>
+
     </div>
   );
 }
--- frontend/client/src/components/Profile.js
+++ frontend/client/src/components/Profile.js
@@ -1,7 +1,17 @@
-function Profile() {
+import Image from 'react-bootstrap/Image'
+import musician from "../images/musician.jpeg"
+import AppContext from "../context/AppContext";
+import {useContext} from "react";
+
+const Profile = () => {
+    const { user } = useContext(AppContext);
     return(
         <div>
             <h1>Profile</h1>
+            <Image style={{width:300, height: 300, marginTop:50}} roundedCircle={true} src={musician}/>
+            <h3>
+                Welcome {user}
+            </h3>
         </div>
     )
 }
