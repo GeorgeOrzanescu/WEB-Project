af2c1694c51d417ecad94fb986c5717dc0d51168
GeorgeOrzanescu <george.asro@gmail.com>
2022-11-09 23:33:59 +0200

    sending dummy data for songs to work in client

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -6,8 +6,26 @@ const getFavouriteSongs = async (req, res, next) => {
   console.log(userId);
   try {
     const user = await _getUserById(userId);
-    console.log("Testing", user);
-    res.send(user);
+    //console.log(user.getFavouriteSongs()); // u get a getter automatically for each field NICE
+    //res.send(await user.getFavouriteSongs());
+    // JUST SEND DUMMY DATA TO WORK IN FRONTEND
+    res.send([
+      {
+        title: "Gone Baby",
+        artist: "Bill Murray",
+        year: "2002",
+      },
+      {
+        title: "Dance Slowly",
+        artist: "Bill Gates",
+        year: "2001",
+      },
+      {
+        title: "Find all truth",
+        artist: "Miki Ra",
+        year: "2090",
+      },
+    ]);
   } catch (error) {
     res.status(404).send("Unable to retrieve songs");
   }
