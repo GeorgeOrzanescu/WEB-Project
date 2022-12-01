82be5835adcb70ec4c1d06339e5cd485e3b44b86
GeorgeOrzanescu <george.asro@gmail.com>
2022-12-01 19:13:31 +0200

    add song to database reflects in list of songs

--- frontend/client/src/services/userService.js
+++ frontend/client/src/services/userService.js
@@ -8,8 +8,23 @@ const getUserSongs = async (userId) => {
   }
 };
 
+const addFavouriteSong = async (userId,data) => {
+  const result = await fetch(__API_URL__ + "songs/" + userId + "/add",{
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  if (result.status === 200) {
+    const data = await result.json();
+    return data;
+  }
+}
+
 const UserService = {
   getUserSongs,
+  addFavouriteSong
 };
 
 export default UserService;
